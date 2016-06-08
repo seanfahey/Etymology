2a109ea1f193fc161728519d005ca409c1de6bf3
sean <sean@seanfahey.org>
2016-06-08 16:48:33 -0500

    Initial commit

---
+++ lambda.js
@@ -0,0 +1,427 @@
+'use strict';
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.9d12620e-b77d-4574-875a-1d4216238838") {
+            context.fail("Invalid Application ID");
+        }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // handle yes/no intent after the user has been prompted
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        } else if ("AMAZON.YesIntent" === intentName) {
+            handleRepeatRequest(intent, session, callback);
+        }
+    }
+
+    // dispatch custom intents to handlers here
+    if ("EtymologyIntent" === intentName) {
+        handleWordRequest(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        handleWordRequest(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        handleWordRequest(intent, session, callback);
+    } else if ("AMAZON.StartOverIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.RepeatIntent" === intentName) {
+        handleRepeatRequest(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        handleGetHelpRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+// ------- Skill specific business logic -------
+
+var ANSWER_COUNT = 4;
+var GAME_LENGTH = 5;
+var CARD_TITLE = "Etymology"; // Be sure to change this for your skill.
+
+function getWelcomeResponse(callback) {
+    var sessionAttributes = {},
+        speechOutput = "What word would you like me to find out about?",
+        shouldEndSession = false,
+        repromptText = "What word would you like me to find out about?";
+
+    speechOutput += repromptText;
+    sessionAttributes = {
+        "speechOutput": repromptText,
+        "repromptText": repromptText
+    };
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleWordRequest(intent, session, callback) {
+    var speechOutput = "";
+    var sessionAttributes = {};
+    var gameInProgress = session.attributes;
+    var answerSlotValid = isAnswerSlotValid(intent);
+
+	speechOutput = 'ok sean, ' + intent.slots.Word.value;
+
+	callback(sessionAttributes,
+		buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+
+/*
+    if (!gameInProgress) {
+        // If the user responded with an answer but there is no game in progress, ask the user
+        // if they want to start a new game. Set a flag to track that we've prompted the user.
+        sessionAttributes.userPromptedToContinue = true;
+        speechOutput = "There is no game in progress. Do you want to start a new game? ";
+        callback(sessionAttributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+    } else if (!answerSlotValid && !userGaveUp) {
+        // If the user provided answer isn't a number > 0 and < ANSWER_COUNT,
+        // return an error message to the user. Remember to guide the user into providing correct values.
+        var reprompt = session.attributes.speechOutput;
+        speechOutput = "Your answer must be a number between 1 and " + ANSWER_COUNT + ". " + reprompt;
+        callback(session.attributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, reprompt, false));
+    } else {
+        var gameQuestions = session.attributes.questions,
+            correctAnswerIndex = parseInt(session.attributes.correctAnswerIndex),
+            currentScore = parseInt(session.attributes.score),
+            currentQuestionIndex = parseInt(session.attributes.currentQuestionIndex),
+            correctAnswerText = session.attributes.correctAnswerText;
+
+        var speechOutputAnalysis = "";
+
+        if (answerSlotValid && parseInt(intent.slots.Word.value) == correctAnswerIndex) {
+            currentScore++;
+            speechOutputAnalysis = "correct. ";
+        } else {
+            if (!userGaveUp) {
+                speechOutputAnalysis = "wrong. "
+            }
+            speechOutputAnalysis += "The correct answer is " + correctAnswerIndex + ": " + correctAnswerText + ". ";
+        }
+        // if currentQuestionIndex is 4, we've reached 5 questions (zero-indexed) and can exit the game session
+        if (currentQuestionIndex == GAME_LENGTH - 1) {
+            speechOutput = userGaveUp ? "" : "That answer is ";
+            speechOutput += speechOutputAnalysis + "You got " + currentScore.toString() + " out of "
+                + GAME_LENGTH.toString() + " questions correct. Thank you for playing!";
+            callback(session.attributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, "", true));
+        } else {
+            currentQuestionIndex += 1;
+            var spokenQuestion = Object.keys(questions[gameQuestions[currentQuestionIndex]])[0];
+            // Generate a random index for the correct answer, from 0 to 3
+            correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
+            var roundAnswers = populateRoundAnswers(gameQuestions, currentQuestionIndex, correctAnswerIndex),
+
+                questionIndexForSpeech = currentQuestionIndex + 1,
+                repromptText = "Question " + questionIndexForSpeech.toString() + ". " + spokenQuestion + " ";
+            for (var i = 0; i < ANSWER_COUNT; i++) {
+                repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". "
+            }
+            speechOutput += userGaveUp ? "" : "That answer is ";
+            speechOutput += speechOutputAnalysis + "Your score is " + currentScore.toString() + ". " + repromptText;
+
+            sessionAttributes = {
+                "speechOutput": repromptText,
+                "repromptText": repromptText,
+                "currentQuestionIndex": currentQuestionIndex,
+                "correctAnswerIndex": correctAnswerIndex + 1,
+                "questions": gameQuestions,
+                "score": currentScore,
+                "correctAnswerText":
+                    questions[gameQuestions[currentQuestionIndex]][Object.keys(questions[gameQuestions[currentQuestionIndex]])[0]][0]
+            };
+            callback(sessionAttributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, false));
+        }
+    }
+    */
+}
+
+function handleRepeatRequest(intent, session, callback) {
+    // Repeat the previous speechOutput and repromptText from the session attributes if available
+    // else start a new game session
+    if (!session.attributes || !session.attributes.speechOutput) {
+        getWelcomeResponse(callback);
+    } else {
+        callback(session.attributes,
+            buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
+    }
+}
+
+/**
+ * Provide a help prompt for the user
+ *
+ * @param intent
+ * @param session
+ * @param callback
+ */
+function handleGetHelpRequest(intent, session, callback) {
+    // Set a flag to track that we're in the Help state.
+    session.attributes.userPromptedToContinue = true;
+
+    var speechOutput = "Ask me about the origin of a word and I will tell you.",
+        repromptText = "Would you like to ask about another word?";
+        var shouldEndSession = false;
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+function isAnswerSlotValid(intent) {
+    var answerSlotFilled = intent.slots && intent.slots.Answer && intent.slots.Answer.value;
+    var answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value));
+    return answerSlotIsInt && parseInt(intent.slots.Answer.value) < (ANSWER_COUNT + 1) && parseInt(intent.slots.Answer.value) > 0;
+}
+
+// ------- Helper functions to build responses -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
+/**
+ * Control events are processed here.
+ * This is called when Alexa requests an action (IE turn off appliance).
+ */
+function handleControl(event, context) {
+
+    /**
+     * Fail the invocation if the header is unexpected. This example only demonstrates
+     * turn on / turn off, hence we are filtering on anything that is not SwitchOnOffRequest.
+     */
+    if (event.header.namespace != 'Control' || event.header.name != 'SwitchOnOffRequest') {
+        context.fail(generateControlError('SwitchOnOffRequest', 'UNSUPPORTED_OPERATION', 'Unrecognized operation'));
+    }
+
+    if (event.header.namespace === 'Control' && event.header.name === 'SwitchOnOffRequest') {
+
+        /**
+         * Retrieve the appliance id and accessToken from the incoming message.
+         */
+        var applianceId = event.payload.appliance.applianceId;
+        var accessToken = event.payload.accessToken.trim();
+        log('applianceId', applianceId);
+
+        /**
+         * Make a remote call to execute the action based on accessToken and the applianceId and the switchControlAction
+         * Some other examples of checks:
+         *	validate the appliance is actually reachable else return TARGET_OFFLINE error
+         *	validate the authentication has not expired else return EXPIRED_ACCESS_TOKEN error
+         * Please see the technical documentation for detailed list of errors
+         */
+        var basePath = '';
+        if (event.payload.switchControlAction === 'TURN_ON') {
+            basePath = REMOTE_CLOUD_BASE_PATH + '/' + applianceId + '/on?access_token=' + accessToken;
+        } else if (event.payload.switchControlAction === 'TURN_OFF') {
+            basePath = REMOTE_CLOUD_BASE_PATH + '/' + applianceId + '/of?access_token=' + accessToken;
+        }
+
+        var options = {
+            hostname: REMOTE_CLOUD_HOSTNAME,
+            port: 443,
+            path: REMOTE_CLOUD_BASE_PATH,
+            headers: {
+                accept: '*/*'
+            }
+        };
+
+        var serverError = function (e) {
+            log('Error', e.message);
+            /**
+             * Craft an error response back to Alexa Smart Home Skill
+             */
+            context.fail(generateControlError('SwitchOnOffRequest', 'DEPENDENT_SERVICE_UNAVAILABLE', 'Unable to connect to server'));
+        };
+
+        var callback = function(response) {
+            var str = '';
+
+            response.on('data', function(chunk) {
+                str += chunk.toString('utf-8');
+            });
+
+            response.on('end', function() {
+                /**
+                 * Test the response from remote endpoint (not shown) and craft a response message
+                 * back to Alexa Smart Home Skill
+                 */
+                log('done with result');
+                var headers = {
+                    namespace: 'Control',
+                    name: 'SwitchOnOffResponse',
+                    payloadVersion: '1'
+                };
+                var payloads = {
+                    success: true
+                };
+                var result = {
+                    header: headers,
+                    payload: payloads
+                };
+                log('Done with result', result);
+                context.succeed(result);
+            });
+
+            response.on('error', serverError);
+        };
+
+        /**
+         * Make an HTTPS call to remote endpoint.
+         */
+        https.get(options, callback)
+            .on('error', serverError).end();
+    }
+}
+
+/**
+ * Utility functions.
+ */
+function log(title, msg) {
+    console.log('*************** ' + title + ' *************');
+    console.log(msg);
+    console.log('*************** ' + title + ' End*************');
+}
+
+function generateControlError(name, code, description) {
+    var headers = {
+        namespace: 'Control',
+        name: name,
+        payloadVersion: '1'
+    };
+
+    var payload = {
+        exception: {
+            code: code,
+            description: description
+        }
+    };
+
+    var result = {
+        header: headers,
+        payload: payload
+    };
+
+    return result;
+}
